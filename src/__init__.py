8e93165efa0e16e50488a0450ed41b778348a00e
jeffreymenchion <jeffrey.menchion@spaceforce.mil>
2024-08-07 16:33:11 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
+#in your notebook cell
+import sys
 
+#path relative to your notebook
+sys.path.insert(0, '../src')
+
+#use if your files are in a package
+sys.path.insert(0, '..')
+
+#autoreload
+%load_ext autoreload
+%autoreload 2
